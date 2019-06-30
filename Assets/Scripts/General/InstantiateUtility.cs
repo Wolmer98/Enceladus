26beaa19f77dd526ce89aad216c1cda980e2a698
Arvid <arvid.enoch@hotmail.com>
2019-06-30 22:21:22 +0100

    Switched out big and medium enemies ragdolls for enemies that break

--- Assets/Scripts/General/InstantiateUtility.cs
+++ Assets/Scripts/General/InstantiateUtility.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class InstantiateUtility : MonoBehaviour
 {
+    private void Start()
+    {
+        FindObjectOfType<WorldGenerator>().OnWorldStart.AddListener(delegate { Init(); });
+    }
+
+    PlayerController player;
+
+    void Init()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
     public void InstantiateObject(GameObject gameObject)
     {
         Instantiate(gameObject, transform.position, transform.rotation);
@@ -11,11 +23,18 @@ public class InstantiateUtility : MonoBehaviour
 
     public void InstantiateRigidbody(GameObject gameObject)
     {
+        if (player == null)
+            player = FindObjectOfType<PlayerController>();
+
+        float damage = player.MainWeapon.WeaponStats.damage;
+
         Rigidbody rb = GetComponent<Rigidbody>();
-        Rigidbody[] rbArray = Instantiate(gameObject, transform.position, transform.rotation).GetComponentsInChildren<Rigidbody>();
+        GameObject instantiatedObject = Instantiate(gameObject, transform.position, transform.rotation);
+        Rigidbody[] rbArray = instantiatedObject.GetComponentsInChildren<Rigidbody>();
         foreach (Rigidbody rigidBody in rbArray)
         {
             rigidBody.velocity = rb.velocity.normalized;
+            rigidBody.AddForce(damage * 0.01f * (player.transform.position - instantiatedObject.transform.position), ForceMode.Impulse);
         }
     }
 }
